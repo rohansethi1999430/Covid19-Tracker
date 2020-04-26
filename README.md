7aa80c4971ae839155ac10fae07134e7c5a23dbc
Rohan <rohansethi1999430@icloud.com>
2020-04-26 14:05:38 +0530

    initial commit

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
+<<<<<<< HEAD
+# Covid19-Tracker
+=======
 # CovidTracker
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.5.
@@ -25,3 +28,4 @@ Run `ng e2e` to execute the end-to-end tests via [Protractor](http://www.protrac
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+>>>>>>> initial commit
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { HomeComponent } from './components/home/home.component';
-import { CountriesComponent } from './components/countries/countries.component';
-import {HttpClientModule} from '@angular/common/http'
+
 @NgModule({
   declarations: [
-    AppComponent,
-    NavbarComponent,
-    HomeComponent,
-    CountriesComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule 
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
