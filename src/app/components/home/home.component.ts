54f495d9ca3b7082e5849f7940a0719b8c81a453
Rohan <rohansethi1999430@icloud.com>
2020-04-27 21:03:08 +0530

    navbar and fetching of data from api

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { DataServiceService } from 'src/app/services/data-service.service';
+import { GlobalDataSummary } from 'src/app/models/global-data';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit {
+  totalConfirmed=0;
+  totalActive=0;
+  totalDeaths=0;
+  totalRecovered=0;
+  globalData:GlobalDataSummary[];
+  constructor(private dataService:DataServiceService) { }
+
+  ngOnInit():void {
+    this.dataService.getGlobalData()
+    .subscribe(
+      {
+        next :(result :any) =>{
+          console.log(result);
+          this.globalData=result;
+          result.forEach(cs=>{
+            if(!Number.isNaN(cs.confirmed))
+            {
+              this.totalActive+=cs.active
+              this.totalConfirmed+=cs.confirmed
+              this.totalDeaths+=cs.deaths
+              this.totalRecovered+=cs.active
+            }
+          })
+        }
+      }
+    )
+  }
+
+}
