27fd4ee35f3d2702ca207f38619591e269c35ea1
Rohan <rohansethi1999430@icloud.com>
2020-05-02 02:24:59 +0530

    Countrywise Display Working

--- src/app/components/countries/countries.component.ts
+++ src/app/components/countries/countries.component.ts
@@ -10,7 +10,10 @@ import { GlobalDataSummary } from 'src/app/models/global-data';
 export class CountriesComponent implements OnInit {
   data:GlobalDataSummary[];
   countries:string[]=[];
-
+  totalConfirmed=0;
+  totalActive=0;
+  totalDeaths=0;
+  totalRecovered=0;
   constructor(private service:DataServiceService) { }
 
   ngOnInit() {
@@ -20,6 +23,18 @@ export class CountriesComponent implements OnInit {
         this.countries.push(cs.country)
       })
     })
+    
+  }
+  updateValues(country : string){
+      console.log(country);
+      this.data.forEach(cs=>{
+        if(cs.country==country){
+          this.totalActive=cs.active
+          this.totalDeaths=cs.deaths
+          this.totalConfirmed=cs.confirmed
+          this.totalRecovered=cs.recovered
+        }
+      })
   }
 
 }
